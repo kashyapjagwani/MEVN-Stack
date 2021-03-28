6d002d791892c0de8608e957502e3a84a9bb4ccd
Kashyap Jagwani <kashyapjagwani@gmail.com>
2021-03-28 12:43:38 +0530

    UI updates

--- src/components/home/MainContent.vue
+++ src/components/home/MainContent.vue
@@ -1,42 +1,48 @@
 <template>
   <!-- Main -->
   <div id="main">
-
     <!-- Welcome Post -->
     <article class="post featured">
       <header class="major">
         <!-- <span class="date">April 25, 2017</span> -->
-        <h2>Welcome to <br>Sparkling Sugar</h2>
-        <b><i>A thoughtful expression of modern craft through good food and warm experiences...</i></b>
+        <h2>Welcome to <br />Sparkling Sugar</h2>
+        <b
+          ><i
+            >A thoughtful expression of modern craft through good food and warm
+            experiences...</i
+          ></b
+        >
       </header>
       <!-- <a href="#" class="image main"><img src="images/pic01.jpg" alt="" /></a> -->
-      <ul class="actions special">
+      <!-- <ul class="actions special">
         <li><a href="#" class="button large">Full Story</a></li>
-      </ul>
+      </ul> -->
       <!-- <div class="box">
         <i>Now accepting Thanksgiving Preorders
           <br>
           <a href="#">Place Order <i class="fa fa-arrow-right" aria-hidden="true"></i></a>
         </i>
       </div> -->
-    </article>    
+    </article>
 
-      <!-- Covid 19 Update -->
-      <article>
-        <div class="box">
-          <header class="major">
-            <h2>COVID-19 Update</h2>
-          </header>
-          <!-- <a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a> -->
-          <i>We are open and offer take-out and have online ordering available. Please stay home if you are feeling any flu like symptoms!
-            <br>
-            Thank you for your continued support!
-          </i>
-        </div>
-        <!-- <ul class="actions special">
+    <!-- Covid 19 Update -->
+    <article>
+      <div class="box">
+        <header class="major">
+          <h2>COVID-19 Update</h2>
+        </header>
+        <!-- <a href="#" class="image fit"><img src="images/pic02.jpg" alt="" /></a> -->
+        <i
+          >We are open and offer take-out and have online ordering available.
+          Please stay home if you are feeling any flu like symptoms!
+          <br />
+          Thank you for your continued support!
+        </i>
+      </div>
+      <!-- <ul class="actions special">
           <li><a href="#" class="button">Full Story</a></li>
         </ul> -->
-      </article>
+    </article>
 
     <h1 style="text-align: center">Our Top Selling Items</h1>
     <section class="posts">
@@ -44,17 +50,21 @@
         <header>
           <h2>It's Loaf Time</h2>
         </header>
-        <a href="#" class="image fit"><img src="../../assets/images/featured-loaf2.jpg" alt="" /></a>
-        <i>Spongy chocolate loaf drizzled with the chocolate of your choice (milk or white)! Just heat it and serve... 
-          <br>Tatses best when served with vanilla icecream!
-          <br>
-          <b>Serves 2</b>  
+        <a href="#" class="image fit"
+          ><img src="../../assets/images/featured-loaf2.jpg" alt=""
+        /></a>
+        <i
+          >Spongy chocolate loaf drizzled with the chocolate of your choice
+          (milk or white)! Just heat it and serve... <br />Tatses best when
+          served with vanilla icecream!
+          <br />
+          <b>Serves 2</b>
         </i>
-        <br>
+        <br />
         <ul class="actions special">
           <li>
             <router-link to="/order" class="button">
-              Order Now 
+              Order Now
               <i class="fa fa-arrow-right" aria-hidden="true"></i>
             </router-link>
           </li>
@@ -64,16 +74,20 @@
         <header>
           <h2>Cupcake it is</h2>
         </header>
-        <a href="#" class="image fit"><img src="../../assets/images/featured-cupcake.jpg" alt="" /></a>
-        <i>Mini cupcake bites with dark chocolate or milk chocolate ganache frosting (as per preference).
-          <br>
+        <a href="#" class="image fit"
+          ><img src="../../assets/images/featured-cupcake.jpg" alt=""
+        /></a>
+        <i
+          >Mini cupcake bites with dark chocolate or milk chocolate ganache
+          frosting (as per preference).
+          <br />
           <b>Serves 1</b>
         </i>
-        <br><br>
+        <br /><br />
         <ul class="actions special">
           <li>
             <router-link to="/order" class="button">
-              Order Now 
+              Order Now
               <i class="fa fa-arrow-right" aria-hidden="true"></i>
             </router-link>
           </li>
@@ -95,16 +109,11 @@
         <a href="#" class="next">Next</a>
       </div> -->
     </footer>
-
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,20 +1,23 @@
 <template>
   <div id="wrapper" class="fade-in">
-    <Header />
+    <!-- <Header /> -->
     <MainContent />
-    </div>
+    <Footer />
+  </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '../components/common/Header'
-import MainContent from '../components/home/MainContent'
+import Header from '../components/common/Header';
+import MainContent from '../components/home/MainContent';
+import Footer from '../components/common/Footer';
 
 export default {
   name: 'Home',
   components: {
     Header,
     MainContent,
-  }
-}
+    Footer,
+  },
+};
 </script>
