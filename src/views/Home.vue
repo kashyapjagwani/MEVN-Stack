77093f295dafecb7f0902e55b3d4634e007629db
Kashyap Jagwani <kashyapjagwani@gmail.com>
2021-03-29 14:37:45 +0530

    UI updates

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,23 +1,17 @@
 <template>
   <div id="wrapper" class="fade-in">
-    <!-- <Header /> -->
     <MainContent />
-    <Footer />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Header from '../components/common/Header';
 import MainContent from '../components/home/MainContent';
-import Footer from '../components/common/Footer';
 
 export default {
   name: 'Home',
   components: {
-    Header,
     MainContent,
-    Footer,
   },
 };
 </script>
